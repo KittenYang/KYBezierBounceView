42463842b57414dde06ba75b5b9bdd191f4704bc
KittenYang <imkittenyang@gmail.com>
2015-02-17 14:12:22 +0800

    first commit

---
+++ KYBezierBounceView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KYBezierBounceView
+//
+//  Created by Kitten Yang on 2/17/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KYBezierBounceView/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  KYBezierBounceView
+//
+//  Created by Kitten Yang on 2/17/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+
+@end
+
+@implementation ViewController
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+
+}
+
+
+
+
+@end
